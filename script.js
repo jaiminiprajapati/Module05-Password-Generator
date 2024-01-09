f59e0f624111f2c168b483bb916bd16598785598
jaiminiprajapati <jaiminihp397@gmail.com>
2024-01-09 17:40:17 +0000

    function getPasswordOptions created in script file

--- script.js
+++ script.js
@@ -87,3 +87,24 @@ var specialCharacters = [
     'Y',
     'Z'
   ];
+
+  let passwordLength = 0;
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  const characterTypes = [];
+  passwordLength = parseInt(prompt("Enter password length between 8 and 128 characters: "));
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+    alert ("Password length should be be between 8 and 128");
+    getPasswordOptions();
+  } 
+  if (confirm("Include lowercase characters?")) characterTypes.push(lowerCasedCharacters);
+  if (confirm("Include uppercase characters?")) characterTypes.push(upperCasedCharacters);
+  if (confirm("Include numeric characters?")) characterTypes.push(numericCharacters);
+  if (confirm("Include special characters?")) characterTypes.push(specialCharacters);
+  if(characterTypes.length === 0){
+    alert("At least one character type should be selected");
+    if (confirm("Try again?")) getPasswordOptions();
+  }
+  return characterTypes;
+}
